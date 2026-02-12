e6ad59007f94d6924286d8549640c628356844aa
LaceySana <laceysanabria@gmail.com>
2026-02-12 13:04:13 -0700

    added js to add an item to html list

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,53 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, qs, setClick } from "./utils.mjs";
 
 loadHeaderFooter();
+
+setClick("#add-item", getNewItem);
+
+
+function getNewItem() {
+    let inputDiv = qs("#input-div");
+    if (!inputDiv) {
+        const listMenu = qs("#list-btns");
+        inputDiv = document.createElement("div");
+        inputDiv.setAttribute("id", "input-div")
+        listMenu.after(inputDiv);
+        
+        const inputLabel = document.createElement("label");
+        inputLabel.textContent = "New Item: ";
+        
+        const itemInput = document.createElement("input");
+        inputLabel.appendChild(itemInput);
+        itemInput.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                addItem();
+            }
+        })
+    
+        const addItem = function () {
+            const newItem = itemInput.value;
+            inputDiv.parentNode.removeChild(inputDiv);
+            
+            const list = qs("#item-list");
+            const li = document.createElement("li");
+            li.innerHTML = `
+            <button class="mark-item">⬜<button> ${newItem}
+            `;
+    
+            list.appendChild(li);
+        }
+        
+        const inputBtn = document.createElement("input");
+        inputBtn.setAttribute("id", "inputSubmit");
+        inputBtn.setAttribute("type", "submit");
+        inputBtn.setAttribute("value", "Add Item");
+    
+        inputDiv.appendChild(inputLabel);
+        inputDiv.appendChild(inputBtn);
+        itemInput.focus();
+        
+        setClick("#inputSubmit", addItem);
+    } else {
+        inputDiv.parentNode.removeChild(inputDiv);
+    }
+}
--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,3 +1,25 @@
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+
 export function renderWithTemplate(template, parentElement, data, callback) {
     parentElement.innerHTML = template;
     if (callback) {
